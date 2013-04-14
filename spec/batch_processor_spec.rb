313e28d04962ab5f8cc120533449ddc7ee890c47
Thomas Egret <thomas.egret@gmail.com>
2013-04-15 08:14:47 +1000

    test

--- spec/batch_processor_spec.rb
+++ spec/batch_processor_spec.rb
@@ -1,7 +1,20 @@
 require_relative '../lib/batch_processor'
 
 describe BatchProcessor do
-  it 'should exit with status 0' do
-    expect(BatchProcessor.new(taxonomy: "taxonomy.xml", content: "destinations.xml", output_dir: "dest").generate_destination_files).to eq 0 
+  before do
+    @bp = BatchProcessor.new(taxonomy: "taxonomy.xml", content: "destinations.xml", output_dir: "dest")
   end
+  
+  it 'should exit with status 0' do
+    expect(@bp.generate_destination_files).to eq 0   
+  end
+
+  # it 'should return true' do 
+  #   @bp.stub(:create_output_dir).and_return(true)
+  #   @bp.stub(:copy_static_files).and_return(true)
+  #   @bp.stub(:move_in_directory).and_return(true)
+  #   @bp.stub(:get_root_nodes).and_return([])
+
+  #   expect(@bp.generate_destination_files).to eq []    
+  # end
 end
